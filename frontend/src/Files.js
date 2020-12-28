a6005df17ce1895a1a2c24aa6fa590c7cf6e8d81
Michael Abah <donsmartez@gmail.com>
2020-12-28 14:25:25 +0100

    adjusted the repo

---
+++ frontend/src/Files.js
@@ -0,0 +1,32 @@
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import React from "react";
+
+export const filesQuery = gql`
+  {
+      files
+  }
+`;
+
+export const Files = () => {
+  const { data, loading } = useQuery(filesQuery);
+console.log(data);
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      {data ? data.files.map(x => (
+        <img
+          style={{ width: 200 }}
+          key={x}
+          src={`http://localhost:4000/images/${x}`}
+          alt={x}
+        /> 
+      )) : (
+            ""
+        )}
+    </div>
+  );
+};
